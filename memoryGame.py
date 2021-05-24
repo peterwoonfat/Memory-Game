8fd8aabbb367625ac22303fc1fdb10b07ea83c9d
Peter Woon-Fat <peterwoonfat@yahoo.ca>
2021-05-24 12:02:24 -0400

    Added more functions to card class related to modifying the status of cards and added more conditions to restrict which cards player can reveal

--- memoryGame.py
+++ memoryGame.py
@@ -3,7 +3,7 @@
 # Pairs of cards are randomly arranged across a grid. They are originally face up for player to see for a few seconds then turned face down
 # and the player must find the matching pairs within the time limit - each matched pair gives 1 point.
 
-# import modules
+# import modules used for memory game
 import random
 
 # declare initialize class for card objects
@@ -13,10 +13,26 @@ class Card:
     def __init__(self, letter):
         self.letter = letter
         self.selected = False
+        self.matched = False
     def set_selected(self):
         self.selected = True
     def set_unselected(self):
         self.selected = False
+    def is_selected(self):
+        if self.selected == True:
+            return True
+        return False
+    def set_matched(self):
+        self.matched = True
+    def is_matched(self):
+        if self.matched == True:
+            return True
+        return False
+    def reveal(self):
+        if self.selected == True:
+            return self.letter
+        return '-'
+
 
 # function initializes 16 cards (8 pairs) with a letter and their position in the grid
 # returns list of cards
@@ -38,28 +54,48 @@ def initialize_cards():
     cardH1 = Card('H')
     cardH2 = Card('H')
     cardList = [cardA1, cardA2, cardB1, cardB2, cardC1, cardC2, cardD1, cardD2, cardE1, cardE2, cardF1, cardF2, cardG1, cardG2, cardH1, cardH2]
-    return set_positions(cardList)
+    return set_positions(cardList, 16)
 
 # function takes list of card objects and randomizes positions in list
-def set_positions(cards):
-    return list(random.sample(cards, 16))
+def set_positions(cards, numCards):
+    return list(random.sample(cards, numCards))
 
 # make function to take player guess
-def get_guess(num):
+def get_guess(cards, num, limit):
     if num == 1:
-        return input('Enter the index of the first card you want to reveal: ')
+        playerGuess = int(input('Enter the index of the first card you want to reveal: '))
+        while playerGuess < 1 or playerGuess > 16 or cards[playerGuess].is_matched():
+            playerGuess = int(input('Enter the index of the first card you want to reveal: '))
+        return playerGuess
     else:
-        return input('Enter the index of the second card you want to reveal: ')
+        playerGuess = int(input('Enter the index of the second card you want to reveal: '))
+        while playerGuess < 1 or playerGuess > 16 or cards[playerGuess].is_selected() or cards[playerGuess].is_matched():
+            playerGuess = int(input('Enter the index of the second card you want to reveal: '))
+        return playerGuess
 
-# function sets specified card to selected
-# takes the list of cards in play and the index of the player chosen card as parameters
-def reveal_card(cards, index):
-    cards[index].setSelected()
+def print_header():
+    headerStr = '''
+    ---Welcome to Peter's Memory Game!---
+    ---There are 8 pairs of cards with matching letters---
+    ---Finding the matching pairs to earn points!---
+    ---Remember you can only turn over 2 cards at a time---
+    '''
+    print(headerStr.center(55))
 
+def print_cards():
+    pass
+
+# main code body, calls other functions to perform operations
 if __name__ == '__main__':
+    print_header()
+    playerPoints = 0
     cardsInPlay = initialize_cards()
-    while len(cardGrid) != 0:
-        guess1 = get_guess(1)
-        reveal_card(guess1)
-        guess2 = get_guess(2)
-        reveal_card(guess2)
+    while len(cardsInPlay) != 0:
+        guess1 = get_guess(cardsInPlay.copy(), 1, 16)
+        cardsInPlay[guess1].set_selected()
+        guess2 = get_guess(cardsInPlay.copy(), 2, 16)
+        cardsInPlay[guess2].set_selected()
+        if cardsInPlay[guess1].letter == cardsInPlay[guess2].letter:
+            cardsInPlay[guess1].set_matched()
+            cardsInPlay[guess2].set_matched()
+            playerPoints = playerPoints + 1
